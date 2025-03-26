3765c0c720ad901cb75aa500d202b088ee14bd29
Alan Yang <yudongyang111@gmail.com>
2025-03-26 15:51:07 -0700

    FireBase config added

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase 配置
+const firebaseConfig = {
+  apiKey: "AIzaSyBAfbwkUozrOjK7nt3ak9skVY98CRrLGeE",
+  authDomain: "uw-social-ae4c7.firebaseapp.com",
+  projectId: "uw-social-ae4c7",
+  storageBucket: "uw-social-ae4c7.firebasestorage.app",
+  messagingSenderId: "719152143746",
+  appId: "1:719152143746:web:bd4379728e72bb9d2c839b",
+  measurementId: "G-FVV15Y8BRH"
+};
+
+// 初始化 Firebase
+firebase.initializeApp(firebaseConfig); 
