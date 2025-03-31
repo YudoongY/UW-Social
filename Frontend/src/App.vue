7618f5560bdfaa3294506c9c31268e831c22de82
Alan Yang <yudongyang111@gmail.com>
2025-03-30 23:52:32 -0700

    更新.gitignore文件，添加更多忽略规则并删除Backend/demo中的.gitignore文件，同时在env.d.ts中声明Vue组件类型

---
+++ Frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-config-provider>
+    <router-view></router-view>
+  </el-config-provider>
+</template>
+
+<script setup lang="ts">
+// 组件逻辑
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f5f7fa;
+}
+</style> 
