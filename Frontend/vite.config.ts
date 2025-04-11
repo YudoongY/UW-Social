4af0bdd8219a615a0f3d17b82a77e5f791e8956f
Alan Yang <yudongyang111@gmail.com>
2025-04-11 00:09:37 -0700

    social-Social

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/UW-social/',
+  base: '/UW-Social/',
   server: {
     proxy: {
       '/api': {
